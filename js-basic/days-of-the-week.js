6b9b51d100dc2be87ed22b266743ac9a4c08da3f
zhujinhong <zjhong6666@gmail.com>
2024-07-24 19:45:38 +0800

    change file name

---
+++ js-basic/days-of-the-week.js
@@ -0,0 +1,27 @@
+var day = 'Sunday';
+switch(day){
+    case 'Monday':
+        console.log('Do something in monday');
+        break;
+    case 'Tuesday':
+        console.log('Do something in tuesday');
+        break;
+    case 'Wednesday':
+        console.log('Do something in wednesay');
+        break;
+    case 'Thursday':
+        console.log('Do somethin in thursday');
+        break;
+    case 'Friday':
+        console.log('Do something in friday');
+        break;
+    case 'Saturday':
+        console.log('Do something in saturday');
+        break;
+    case 'Sunday':
+        console.log('Do something in sunday');
+        break;
+    default:
+        //this block will run if no condition matchs
+        console.log('There are is no such day');
+}
