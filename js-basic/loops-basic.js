8f3f29f5d10b9bbbef7cbe2506aced6e2ed04adb
zhujinhong <zjhong6666@gmail.com>
2024-07-24 20:04:39 +0800

    loop basic

---
+++ js-basic/loops-basic.js
@@ -0,0 +1,17 @@
+for (var i = 1; i <= 5; i++) {
+    console.log(i);
+}
+console.log('First for loop\n');
+
+for (var i = 5; i > 0; i--) {
+    console.log(i);
+}
+console.log('Second for loop\n');
+
+var i = 1;
+while (i < 6) {
+    console.log(i);
+    i++;
+}
+console.log('Third while loop\n')
+
