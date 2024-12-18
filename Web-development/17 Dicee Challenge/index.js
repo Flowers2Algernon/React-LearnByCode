a1e50c4b28cfbcd6e729d2afc61b6ca44a4d857f
jinhongzhu <zjhong6666@gmail.com>
2024-12-18 21:11:59 +0800

    finish

--- Web-development/17 Dicee Challenge/index.js
+++ Web-development/17 Dicee Challenge/index.js
@@ -6,3 +6,10 @@ var randomNumber2 = Math.floor(Math.random()*6+1);
 var randomDiceImage2 = "dice" + randomNumber2 + ".png";
 var randomImageSource2 = "images/" + randomDiceImage2;
 document.querySelector(".img2").setAttribute("src", randomImageSource2);
+if (randomNumber1 > randomNumber2 ){
+    document.querySelector("h1").innerHTML = "Player 1 Wins!";
+} else if (randomNumber1 < randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 2 Wins!";
+} else {
+    document.querySelector("h1").innerHTML = "Draw!";
+}
