1d7a8cc24107c5798b3cb92fe104d23a88ab34e2
jinhongzhu <zjhong6666@gmail.com>
2024-12-18 20:29:10 +0800

    update js change css

--- Web-development/16.1 DOM Challenge Starting Files/index.js
+++ Web-development/16.1 DOM Challenge Starting Files/index.js
@@ -1,2 +1,6 @@
 document.querySelector("ul a").style.color = "red";
 document.querySelector("button").style.backgroundColor = "yellow";
+document.querySelector("body > h1").classList.add("huge")
+// document.querySelector("h1").classList.toggle("huge")
+// document.querySelector("h1").classList.toggle("huge")
+// toggle means if it is there then remove it and if it is not there then add it    
