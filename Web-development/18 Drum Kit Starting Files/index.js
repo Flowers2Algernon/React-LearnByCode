a40e6b6caae3731881bec5f52f82ccb14838c1a3
jinhongzhu <zjhong6666@gmail.com>
2024-12-21 20:56:32 +0800

    update js for simple function call

--- Web-development/18 Drum Kit Starting Files/index.js
+++ Web-development/18 Drum Kit Starting Files/index.js
@@ -1,42 +1,15 @@
 for (var i=0;i<document.querySelectorAll(".set > button").length;i++){
     document.querySelectorAll("button")[i].addEventListener("click", function(){
-        switch(this.innerHTML){
-            case "w":
-                var audio = new Audio('sounds/tom-1.mp3');
-                audio.play();
-                break;
-            case "a":
-                var audio = new Audio('sounds/tom-2.mp3');
-                audio.play();
-                break;
-            case "s":
-                var audio = new Audio('sounds/tom-3.mp3');
-                audio.play();
-                break;
-            case "d":
-                var audio = new Audio('sounds/tom-4.mp3');
-                audio.play();
-                break;
-            case "j":
-                var audio = new Audio('sounds/snare.mp3');
-                audio.play();
-                break;
-            case "k":
-                var audio = new Audio('sounds/crash.mp3');
-                audio.play();
-                break;
-            case "l":
-                var audio = new Audio('sounds/kick-bass.mp3');
-                audio.play();
-                break;
-            default:
-                console.log(this.innerHTML);
-        }
+        makeSound(this.innerHTML);
     });
 };
 
 document.addEventListener("keydown", function(event){
-    switch(event.key){
+    makeSound(event.key);
+})
+
+function makeSound(key){
+    switch(key){
         case "w":
             var audio = new Audio('sounds/tom-1.mp3');
             audio.play();
@@ -66,6 +39,6 @@ document.addEventListener("keydown", function(event){
             audio.play();
             break;
         default:
-            console.log(event.key);
+            console.log(key);
     }
-})
+}
