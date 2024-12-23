b6f762c014e05ee63a6c182f13159cccd1e077c1
jinhongzhu <zjhong6666@gmail.com>
2024-12-23 20:34:25 +0800

    update step 1 - 3

--- Web-development/20 Simon Game Challenge Starting Files/game.js
+++ Web-development/20 Simon Game Challenge Starting Files/game.js
@@ -2,6 +2,9 @@ var gamePattern = [];
 var buttonColours = ["red", "blue", "green", "yellow"];
 function nextSequence(){
     var randomNumber = Math.floor(Math.random()*4);
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+    $("#"+randomChosenColour).fadeOut(200).fadeIn(200);
+    new Audio("./sounds/"+randomChosenColour+".mp3").play();
 }
-var randomChosenColour = buttonColours[randomNumber];
-gamePattern.push(randomChosenColour);
+$("btn").on("click",nextSequence());
