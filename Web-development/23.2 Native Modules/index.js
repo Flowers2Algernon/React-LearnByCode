4331b702234b706874200ae651a661a9e30c1f22
jinhongzhu <zjhong6666@gmail.com>
2024-12-24 15:45:31 +0800

    before challenge

---
+++ Web-development/23.2 Native Modules/index.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+
+// fs.writeFileSync("message.txt","Hello from node.js", (err) => {
+//     if (err) throw err;
+//     console.log('successfully added message to file'); 
+// });
