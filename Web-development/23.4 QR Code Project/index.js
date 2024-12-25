f0d962555b8b7bb9348b22bbadf2d7c3087ba127
jinhongzhu <zjhong6666@gmail.com>
2024-12-25 14:02:07 +0800

    update use txt file to save user input

--- Web-development/23.4 QR Code Project/index.js
+++ Web-development/23.4 QR Code Project/index.js
@@ -6,7 +6,14 @@
 import inquirer from 'inquirer';
 import qr from 'qr-image';
 import { input } from '@inquirer/prompts';
+import fs from 'fs';
 
 const answer = await input({ message: 'Enter your url' });
 console.log(answer);
 var qr_svg = qr.image(answer, { type: 'svg' });
+
+
+fs.appendFile('url.txt', answer+'\n', function (err) {
+  if (err) throw err;
+  console.log('Saved!');
+});
