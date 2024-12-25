f3cb104cd5559959d13c09d12531af3eaf30d1a7
jinhongzhu <zjhong6666@gmail.com>
2024-12-25 15:30:44 +0800

    update nodemon

--- Web-development/Backend/3.2 HTTP Requests/index.js
+++ Web-development/Backend/3.2 HTTP Requests/index.js
@@ -3,7 +3,8 @@ import express from 'express';
 const app = express();
 const port = 3001;
 app.get('/', (req,res)=>{
-    res.send('Hello from 3.2 HTTP Requests');
+    res.send('<h1>Hello from 3.2 HTTP Requests</h1>');
+    console.log(req.rawHeaders);
 })
 
 app.listen(port, ()=>{
