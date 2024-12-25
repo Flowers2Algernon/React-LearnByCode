35672bc5bbbadd558696c28c3a02dba8cc1d9ff3
jinhongzhu <zjhong6666@gmail.com>
2024-12-25 15:17:47 +0800

    add new serve

---
+++ Web-development/Backend/3.2 HTTP Requests/index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+const port = 3001;
+app.get('/', (req,res)=>{
+    res.send('Hello from 3.2 HTTP Requests');
+})
+
+app.listen(port, ()=>{
+    console.log(`Server is running on port ${port}`);
+})
