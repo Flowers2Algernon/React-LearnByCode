4c8948cb64bf3c08e237862d526792a6c2187d52
jinhongzhu <zjhong6666@gmail.com>
2024-12-28 15:08:34 +0800

    finish esj basic

--- Web-development/Backend/4.0 EJS/index.js
+++ Web-development/Backend/4.0 EJS/index.js
@@ -4,14 +4,15 @@ const port = 3000;
 const app = express();
 
 
-const template = '<h1>Hey! Its a <%= daytime %>, its time to <%= activity %></h1>';
+const template = '<h1>Hey! Its a <%= daytime %>, its time to <%= activity %>!</h1>';
 const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const date = new Date();
 const dayName = weekdays[date.getDay()];
+let data;
 if (dayName === "Sunday" || dayName === "Saturday") {
-    const data = {daytime: dayName, activity: "have fun"};
+    data = {daytime: dayName, activity: "have fun"};
 } else {
-    const data = {daytime: dayName, activity: "work hard"};
+    data = {daytime: dayName, activity: "work hard"};
 }
 const html = ejs.render(template, data);
 
