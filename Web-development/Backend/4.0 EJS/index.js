98d2191905abd31d130748d74ab2f1f412561103
jinhongzhu <zjhong6666@gmail.com>
2024-12-28 15:28:16 +0800

    update ejs basic usage - use external ejs file and pass value to it

--- Web-development/Backend/4.0 EJS/index.js
+++ Web-development/Backend/4.0 EJS/index.js
@@ -3,8 +3,6 @@ import ejs from 'ejs';
 const port = 3000;
 const app = express();
 
-
-const template = '<h1>Hey! Its a <%= daytime %>, its time to <%= activity %>!</h1>';
 const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const date = new Date();
 const dayName = weekdays[date.getDay()];
@@ -14,10 +12,14 @@ if (dayName === "Sunday" || dayName === "Saturday") {
 } else {
     data = {daytime: dayName, activity: "work hard"};
 }
-const html = ejs.render(template, data);
+
+
 
 app.get('/', (req, res) => {
-    res.send(html);
+    res.render('index.ejs',{
+        daytime: dayName,
+        activity: data.activity
+    })
 });
 
 app.listen(port, () => {
